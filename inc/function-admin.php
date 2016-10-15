e25a8469c072b9a2528ea5fd608cca9c0c0be65a
NemanjaMandic <nemanjans84@gmail.com>
2016-10-15 03:34:09 +0200

    register_setting

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -8,11 +8,21 @@ ADMIN PAGE
 
 function mojo_add_admin_page(){
     
-    add_menu_page('Mojo Theme Options', 'Mojo', 'manage_options', 'nemus-mojo', 'mojo_rising_create_page', get_template_directory_uri() . '/img/sunset-icon.png', 110);
+    //Generate Mojo Admin Page
+    add_menu_page( 'Mojo Theme Options', 'Mojo', 'manage_options', 'nemus_mojo', 'mojo_rising_create_page', get_template_directory_uri() . '/img/sunset-icon.png', 110 );
+    
+    //Generate sub page
+    add_submenu_page( 'nemus_mojo', 'Mojo Theme Options', 'General', 'manage_options', 'nemus_mojo', 'mojo_rising_create_page' );
+    
+    add_submenu_page( 'nemus_mojo', 'Mojo CSS Options', 'Custom CSS', 'manage_options', 'nemus_mojo_css', 'mojo_rising_settings_page' );
 }
 
 add_action( 'admin_menu', 'mojo_add_admin_page' );
 
+function mojo_rising_settings_page(){
+    
+    require_once( get_template_directory() . '/inc/templates/mojo-admin.php');
+}
 function mojo_rising_create_page(){
     echo '<h1>Sisaj karinu</h1>';
 }
---
+++ inc/templates/mojo-admin.php
@@ -0,0 +1,4 @@
+<h1>Jebem ti materesinu</h1>
+
+
+<?php bloginfo( 'name' ); ?>
