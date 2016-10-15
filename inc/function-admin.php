2f85566a899dd0e11c2ab459e5e257ec0955dea8
NemanjaMandic <nemanjans84@gmail.com>
2016-10-15 22:21:08 +0200

    social media icons

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -24,13 +24,50 @@ add_action( 'admin_menu', 'mojo_add_admin_page' );
 
 function mojo_custom_settings(){
     register_setting( 'mojo-settings-group', 'first_name' );
+    register_setting( 'mojo-settings-group', 'last_name' );
+    register_setting( 'mojo-settings-group', 'twitter_handler', 'mojo_sanitize_twitter_handler' );
+    register_setting( 'mojo-settings-group', 'facebook_handler' );
+    register_setting( 'mojo-settings-group', 'gplus_handler' );
+    
     add_settings_section( 'mojo-sidebar-options', 'Sidebar Options', 'mojo_sidebar_options', 'nemus_mojo' );
-    add_settings_field( 'sidebar-name', 'First Name', 'mojo_sidebar_name', 'nemus_mojo', 'mojo-sidebar-options' );
+    add_settings_field( 'sidebar-name', 'Full Name', 'mojo_sidebar_name', 'nemus_mojo', 'mojo-sidebar-options' );
+    add_settings_field( 'sidebar-twitter', 'Twitter Handler', 'mojo_sidebar_twitter', 'nemus_mojo', 'mojo-sidebar-options' );
+    add_settings_field( 'sidebar-facebook', 'Facebook Handler', 'mojo_sidebar_facebook', 'nemus_mojo', 'mojo-sidebar-options' );
+    add_settings_field( 'sidebar-gplus', 'Google+ Handler', 'mojo_sidebar_gplus', 'nemus_mojo', 'mojo-sidebar-options' );
+    
 }
 
+function mojo_sidebar_twitter(){
+    
+    $twitter = esc_attr(get_option( 'twitter_handler' ));
+    
+    echo '<input type="text" name="twitter_handler" value="'. $twitter .'" placeholder="Twitter Handler" />';
+}
+
+function mojo_sidebar_facebook(){
+    
+    $facebook = esc_attr(get_option( 'facebook_handler' ));
+    
+    echo '<input type="text" name="facebook_handler" value="'. $facebook .'" placeholder="Facebook Handler" />';
+}
+
+function mojo_sidebar_gplus(){
+    
+    $gplus = esc_attr(get_option( 'gplus_handler' ));
+    
+    echo '<input type="text" name="gplus_handler" value="'. $gplus .'" placeholder="Google+ Handler" />';
+}
+
+//Sanitization settings
+function mojo_sanitize_twitter_handler(){
+    
+}
 function mojo_sidebar_name(){
+    
     $firstName = esc_attr(get_option( 'first_name' ));
-    echo '<input type="text" name="first_name" value="'. $firstName .'" placeholder="First Name" />';
+    $lastName = esc_attr(get_option( 'last_name' ));
+    
+    echo '<input type="text" name="first_name" value="'. $firstName .'" placeholder="First Name" /> <input type="text" name="last_name" value="'. $lastName .'" placeholder="Last Name" />';
 }
 function mojo_sidebar_options(){
     echo "Kustomajz svoj sajdbar informajsn";
