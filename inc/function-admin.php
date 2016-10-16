61a4887c589805766e132dd89bac031ff0816b02
NemanjaMandic <nemanjans84@gmail.com>
2016-10-16 19:46:44 +0200

    enqueue scripts

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -25,16 +25,23 @@ add_action( 'admin_menu', 'mojo_add_admin_page' );
 function mojo_custom_settings(){
     register_setting( 'mojo-settings-group', 'first_name' );
     register_setting( 'mojo-settings-group', 'last_name' );
+    register_setting( 'mojo-settings-group', 'user_description' );
     register_setting( 'mojo-settings-group', 'twitter_handler', 'mojo_sanitize_twitter_handler' );
     register_setting( 'mojo-settings-group', 'facebook_handler' );
     register_setting( 'mojo-settings-group', 'gplus_handler' );
     
     add_settings_section( 'mojo-sidebar-options', 'Sidebar Options', 'mojo_sidebar_options', 'nemus_mojo' );
     add_settings_field( 'sidebar-name', 'Full Name', 'mojo_sidebar_name', 'nemus_mojo', 'mojo-sidebar-options' );
+     add_settings_field( 'sidebar-description', 'Description', 'mojo_sidebar_description', 'nemus_mojo', 'mojo-sidebar-options' );
     add_settings_field( 'sidebar-twitter', 'Twitter Handler', 'mojo_sidebar_twitter', 'nemus_mojo', 'mojo-sidebar-options' );
     add_settings_field( 'sidebar-facebook', 'Facebook Handler', 'mojo_sidebar_facebook', 'nemus_mojo', 'mojo-sidebar-options' );
     add_settings_field( 'sidebar-gplus', 'Google+ Handler', 'mojo_sidebar_gplus', 'nemus_mojo', 'mojo-sidebar-options' );
     
+}
+
+function mojo_sidebar_description(){
+    $description = esc_attr( get_option( 'user_description' ) );
+    echo '<input type="text" name="user_description" value="'. $description .'" placeholder="User Description" /><p class="description">Write something smart.</p>';
 }
 
 function mojo_sidebar_twitter(){
@@ -48,7 +55,7 @@ function mojo_sidebar_facebook(){
     
     $facebook = esc_attr(get_option( 'facebook_handler' ));
     
-    echo '<input type="text" name="facebook_handler" value="'. $facebook .'" placeholder="Facebook Handler" />';
+    echo '<input type="text" name="facebook_handler" value="'. $facebook .'" placeholder="Facebook Handler" /><p class="description">Input your Twitter username, without @</p>';
 }
 
 function mojo_sidebar_gplus(){
@@ -59,8 +66,11 @@ function mojo_sidebar_gplus(){
 }
 
 //Sanitization settings
-function mojo_sanitize_twitter_handler(){
-    
+function mojo_sanitize_twitter_handler( $input ){
+    $output = sanitize_text_field( $input );
+    $output = str_replace('@', '', $output);
+    return $output;
+
 }
 function mojo_sidebar_name(){
     
