71eea449865b183e707fbb8fb243bb15d8a99337
NemanjaMandic <nemanjans84@gmail.com>
2016-10-17 04:02:20 +0200

    profile picture

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -19,6 +19,11 @@ function mojo_load_admin_scripts( $hook ){
     
     wp_register_style( 'mojo_admin', get_template_directory_uri() . '/css/mojo.admin.css', array(), '1.0.0', 'all' );
     wp_enqueue_style( 'mojo_admin' );
+    
+    wp_enqueue_media();
+    
+    wp_register_script( 'mojo-admin-script', get_template_directory_uri() . '/js/mojo.admin.js', array('jquery'), '1.0.0', true );
+    wp_enqueue_script( 'mojo-admin-script' );
 }
 
 add_action( 'admin_enqueue_scripts', 'mojo_load_admin_scripts' );
