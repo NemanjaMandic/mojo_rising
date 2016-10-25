cf7a6e3be0766ee750a98980915069159e2186ca
NemanjaMandic <nemanjans84@gmail.com>
2016-10-25 02:43:25 +0200

    starting with frontend

--- header.php
+++ header.php
@@ -1,1 +1,14 @@
+<?php
+
+    /*
+        This is the template for the header
+    */
+
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>"
+</head>
+</html>
 <?php wp_head(); ?>
