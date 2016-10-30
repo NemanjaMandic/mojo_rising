50418dacec7b4ba0bbbc2d4c4b816cd1f6bb430f
NemanjaMandic <nemanjans84@gmail.com>
2016-10-30 04:29:42 +0100

    add header image

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width+device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <?php if( is_singular() && pings_open( get_queried_object() ) ) : ?>
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
@@ -19,3 +19,17 @@
 <body <?php body_class(); ?>>
     
 
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-xs-12">
+            
+            <div class="header-container text-center" style="background-image: url(<?php header_image(); ?>)">
+               
+               <div class="header-content"></div><!-- .header-content -->
+               <div class="nav-container"></div><!-- .nav-container -->
+                
+            </div><!-- .custom-header -->
+            
+        </div><!-- .col-xs-12 -->
+    </div><!-- .row -->
+</div><!-- .container-fluid -->
