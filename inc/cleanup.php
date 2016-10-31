9e2d085afb8def1a5af837e6adfd6c29abca13bc
NemanjaMandic <nemanjans84@gmail.com>
2016-10-31 20:20:10 +0100

    adding sass

--- inc/cleanup.php
+++ inc/cleanup.php
@@ -8,6 +8,7 @@ REMOVE GENERATOR VERSION NMUBER
 
 */
 
+//Remove version string from js
 function mojo_remove_wp_version_string( $src ){
     global $wp_version;
     
@@ -18,3 +19,13 @@ function mojo_remove_wp_version_string( $src ){
     
     return $src;
 }
+
+add_filter( 'script_loader_src', 'mojo_remove_wp_version_string' );
+add_filter( 'style_loader_src', 'mojo_remove_wp_version_string' );
+
+//remove metatag generator from header
+function mojo_remove_meta_version(){
+    return '';
+}
+
+add_filter( 'the_generator', 'mojo_remove_meta_version' );
