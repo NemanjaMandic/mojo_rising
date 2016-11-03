dd3f124f18f9f265ca93d17620a28f7ea168abfe
NemanjaMandic <nemanjans84@gmail.com>
2016-11-03 03:49:49 +0100

    mojo posted meta

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -46,7 +46,28 @@ add_action( 'after_setup_theme', 'mojo_register_nav_menu' );
 */
 
 function mojo_posted_meta(){
-    return 'category name and publishing time';
+    
+    $posted_on = human_time_diff( get_the_time('U'), current_time('timestamp') . ' ago');
+    
+    $categories = get_the_category();
+    $separator = ', ';
+    $output = '';
+    $i = 1;
+    
+    if( ! empty( $categories ) ) :
+       
+        foreach( $categories as $category ) :
+    
+            if( $i > 1 ): $output .= $separator; endif;
+            
+            $output .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" alt="' . esc_attr( 'View all posts in %s', $category->name ) . '"> '.  esc_html( $category->name ) . '</a>';
+        
+         $i++;
+    
+        endforeach;
+    endif;
+    
+    return '<span class="posted-on"> Posted <a href="' . esc_url( get_permalink() ) .'">' .  $posted_on . '</a> ago</span><span class="posted-in">' .  $output . '</span>';
 }
 
 function mojo_posted_footer(){
