3afe99b380cb715d6d3ef050f32ad788f097403d
NemanjaMandic <nemanjans84@gmail.com>
2016-11-04 04:30:13 +0100

    standard featured

--- template-parts/content.php
+++ template-parts/content.php
@@ -23,8 +23,9 @@
         <?php if ( has_post_thumbnail() ): 
             $featured_image = wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() ) );
         ?>
-        
+        <a class="standard-featured-link" href="<?php the_permalink(); ?>">
             <div class="standard-featured background-image" style="background-image: url(<?php echo $featured_image; ?>)"></div>
+        </a>
         <?php endif; ?>
         
         <div class="entry-excerpt">
